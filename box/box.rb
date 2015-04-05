3d988bae98b6b8e18d7f8f94d2bf1d7b6ce47032
Mathieu Lecarme <mlecarme@bearstech.com>
2015-04-06 00:10:23 +0200

    Fix: buffered read.

--- box/box.rb
+++ box/box.rb
@@ -19,7 +19,14 @@ UNIXServer.open(socket_path) do |serv|
       else
         # FIXME raise if size is too huge
 
-        template = s.recv(size)
+        all_data = []
+        while true
+          blob = s.recv(size)
+          all_data << blob
+          size -= blob.length
+          break if size == 0
+        end
+        template = all_data.join
         # FIXME handling UTF8
         haml_engine = Haml::Engine.new(template)
         output = haml_engine.render
