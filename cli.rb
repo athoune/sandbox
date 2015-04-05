128c04cee13735bfb108d8c5f6d584de54bda76a
Mathieu Lecarme <mlecarme@bearstech.com>
2015-04-06 00:20:37 +0200

    Timeout and buffered read.

--- cli.rb
+++ cli.rb
@@ -3,15 +3,29 @@
 # This is just a debug tool
 #
 require 'socket'
+require 'timeout'
 
 tpl = STDIN.read
 socket_path = ARGV[0]
 
 s = UNIXSocket.new(socket_path)
 
-# FIXME handling UTF8
-s.puts [tpl.length].pack("L")
-s.puts tpl
-size = s.recv(4).unpack("L")[0]
+status = Timeout::timeout(5) do
+  # FIXME handling UTF8
+  s.puts [tpl.length].pack("L")
+  s.puts tpl
+  size = s.recv(4).unpack("L")[0]
 
-p s.recv(size) if not size.nil?
+  if size
+    all_data = []
+    while true
+      blob = s.recv(size)
+      all_data << blob
+      size -= blob.length
+      break if size == 0
+    end
+    all_data.join
+  end
+end
+
+p status
